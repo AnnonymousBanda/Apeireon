436073ec20f044981545ad59c8423ee222f34b5c
Ankit Bhagat <annonymous007banda@gmail.com>
2024-03-24 08:35:46 +0530

    Added scrollInto behaviour

--- index.js
+++ index.js
@@ -1,91 +1,12 @@
-const eventInfo = [
-	[
-		{
-			src: 'img/poster/speedrun.jpg',
-			title: 'Event 1',
-			time: '10:00',
-			date: '14.April.2024',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-		},
-		{
-			src: 'img/poster/speedrun.jpg',
-			title: 'Event 2',
-			time: '12:30',
-			date: '15.April.2024',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-		},
-		{
-			src: 'img/poster/speedrun.jpg',
-			title: 'Event 3',
-			time: '15:00',
-			date: '16.April.2024',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-		},
-	],
-	[
-		{
-			src: 'img/poster/speedrun.jpg',
-			title: 'Event 4',
-			time: '18:30',
-			date: '17.April.2024',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-		},
-		{
-			src: 'img/poster/speedrun.jpg',
-			title: 'Event 5',
-			time: '21:00',
-			date: '18.April.2024',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-		},
-		{
-			src: 'img/poster/speedrun.jpg',
-			title: 'Event 6',
-			time: '09:45',
-			date: '19.April.2024',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-		},
-	],
-	[
-		{
-			src: 'img/poster/speedrun.jpg',
-			title: 'Event 7',
-			time: '14:15',
-			date: '20.April.2024',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-		},
-		{
-			src: 'img/poster/speedrun.jpg',
-			title: 'Event 8',
-			time: '17:30',
-			date: '21.April.2024',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-		},
-		{
-			src: 'img/poster/speedrun.jpg',
-			title: 'Event 9',
-			time: '19:45',
-			date: '22.April.2024',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-		},
-	],
-]
+import eventInfo from './eventInfo.json' assert {type:'json'}; 
 
+//element rendering tool
 const createElement = (element) => {
 	const tempDiv = document.createElement('div')
 	tempDiv.innerHTML = element
 
 	return tempDiv.firstChild
 }
-
 const renderElement = (attributes, parent) => {
 	const parentElement = document.querySelector(parent)
 
@@ -98,6 +19,8 @@ const renderElement = (attributes, parent) => {
 	})
 }
 
+
+//event tabbed links behaviour
 document.querySelector('.tab-links').addEventListener('click', (e) => {
 	e.preventDefault()
 	if (e.target.tagName !== 'A' || e.target.classList.contains('selected'))
@@ -114,5 +37,18 @@ document.querySelector('.tab-links').addEventListener('click', (e) => {
 	document.querySelectorAll('#events .container *').forEach((x) => x.remove())
 	renderElement(eventInfo[id.charAt(id.length - 1) - 1], '#events .container')
 })
-
 renderElement(eventInfo[0], '#events .container')
+
+
+//navigation link behaviour
+document.querySelectorAll('nav .links, .footer .links').forEach((x)=>{
+	x.addEventListener('click',(e)=>{
+		const id=e.target.getAttribute('href');
+		if(!id||id==='#hackmatrix')
+			return;
+
+		e.preventDefault();
+		document.querySelector(id).scrollIntoView({ behavior: 'smooth', block: 'start' });
+	});
+}) ;
+
